17b7331c61e840ef1bcbc1fc970d57ad0845b55f
Christopher Poole <mail@christopherpoole.net>
2015-04-06 18:25:32 +1000

    Using TVTK to add data to the mlab rendering pipeline, rather than having to converting it to a numpy array first.

--- animatedicom.py
+++ animatedicom.py
@@ -3,93 +3,80 @@
 # Author: Iwan Cornelius <iwan@2mrd.com.au>
 # Date:   1 April 2015
 
+
 import argparse
 import os
 
 import vtk
-import numpy as np
+import tvtk
 
-from vtk.util import numpy_support
 from mayavi import mlab
-from matplotlib import pyplot, cm
 
 
 # Process command line arguments.
 parser = argparse.ArgumentParser()
 
 parser.add_argument("--dicomdir",
-                    type=str,
-                    default="./",
-                    help="The directory containing the dicom series (default:./)")
+        type=str,
+        default="./",
+        help="The directory containing the dicom series (default:./)")
 
 parser.add_argument("--output",
-                    type=str,
-                    default="./animation/",
-                    help="The generated animation output directory (default:./animation/)")
+        type=str,
+        default="./animation/",
+        help="The generated animation output directory (default:./animation/)")
 
 parser.add_argument("--animate",
-                    action="store_true",
-                    default=False,
-                    help="Animate camera rotation about the azimuthal axis and save images to output directory")
+        action="store_true",
+        default=False,
+        help="Animate camera rotation about the azimuthal axis and save each frame")
 
 args = parser.parse_args()
 
 # VTK is used to read in the DICOM data series. By default will look in
-# current directory.
+# the current directory.
 reader = vtk.vtkDICOMImageReader()
 reader.SetDirectoryName(args.dicomdir)
 reader.Update()
 
-# We create numpy arrays to store coordinates of each voxel, accounting for
-# varying pixel size in each direction. This won't work if the pixel spacing
-# varies between slices
-_extent = reader.GetDataExtent()
-ConstPixelDims = [_extent[1]-_extent[0]+1, _extent[3]-_extent[2]+1, _extent[5]-_extent[4]+1]
-ConstPixelSpacing = reader.GetPixelSpacing()
-x = np.arange(0.0, (ConstPixelDims[0]+1)*ConstPixelSpacing[0], ConstPixelSpacing[0])
-y = np.arange(0.0, (ConstPixelDims[1]+1)*ConstPixelSpacing[1], ConstPixelSpacing[1])
-z = np.arange(0.0, (ConstPixelDims[2]+1)*ConstPixelSpacing[2], ConstPixelSpacing[2])
-yv,xv,zv = np.meshgrid(y[1:],x[1:],z[1:])
+# Convert the VTK object to a TVTK object. This is what mayavi uses.
+data = tvtk.api.tvtk.to_tvtk(reader.GetOutput())
 
-# Convert the DICOM data to a numpy array.
-imageData = reader.GetOutput()
-pointData = imageData.GetPointData()
-assert (pointData.GetNumberOfArrays()==1)
-arrayData = pointData.GetArray(0)
-ArrayDicom = numpy_support.vtk_to_numpy(arrayData)
-ArrayDicom = ArrayDicom.reshape(ConstPixelDims, order='F')
+# Create our viewing window.
+fig = mlab.figure(size=(640, 360), bgcolor=(1,1,1))
 
-finalwidth = 640
-finalheight = 360
+# Show the data by adding it to the pipeline, and using a
+# matplotlib colour map name.
+mlab.pipeline.contour_surface(data,
+        contours=3, colormap="Oranges", opacity=0.75)
 
-# Show the data using mlab from mayavi, and the matplotlib colourmap of choice.
-fig = mlab.figure(size=(finalwidth,finalheight), bgcolor=(1,1,1))
-mlab.contour3d(xv, yv, zv, ArrayDicom,
-               contours=4, transparent=True, colormap="Oranges")
-
-# If animate flag has been set; we create the output directory if it
-# doesn't already exist.
+# Perform our rotation animation. 
 if args.animate is True:
-    odir = args.output+"/png"
+
+    # We create the output directory if it doesn't already exist.
+    odir = os.path.join(args.output, "png")
     if not os.path.exists(odir): os.makedirs(odir)
-    
-    # Configure the number of frames we require.
-    engine = mlab.get_engine()
-    scene = engine.current_scene
-    nframes = 100
-    increment = np.float(360.0/nframes)
-    
+   
     # Tweak these to values to set the starting angles of elevation and
     # azimuth. If you run interactively, you can type mlab.view() to get
     # current settings of the camera.
-    mlab.view(0,0)
+    mlab.view(0, 0)
     mlab.view(distance=600)
-    mlab.view(focalpoint=(100,150,80))
+    mlab.view(focalpoint=(100, 150, 80))
 
-    for i in np.arange(nframes):
-        print("frame number ", i)
-        scene.scene.camera.azimuth(increment)
-        scene.scene.render()
-        scene.scene.save_png(odir+"/anim"+np.str(i).zfill(2)+".png")
+    # Configure the number of frames we require.
+    frames = 100
+    increment = 360.0 / frames
 
+    for i in range(frames):
+        print("Rendering frame number: {0}".format(i))
+        fig.scene.camera.azimuth(increment)
+        fig.scene.render()
+        
+        # Save the current frame.
+        filename = os.path.join(odir, "frame{:03}.png".format(i))
+        #scene.scene.save_png(filename)
+
+# Allow interaction with the viewer.
+mlab.show()
 
